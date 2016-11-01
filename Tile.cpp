23412951e1c0039929bd5e3f6d7c5c4409f6e07e
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-01 17:02:14 -0400

    Add overloaded tile constructor

--- Tile.cpp
+++ Tile.cpp
@@ -5,6 +5,15 @@ Tile::Tile(){
 
 }
 
+Tile::Tile(bool bHasMonastery, bool bRoadsEnd, bool bCitiesAreIndependent, bool bHasShield, int Edges[])
+{
+	this->hasMonastery = bHasMonastery;
+	this->roadsEnd = bRoadsEnd;
+	this->citiesAreIndependent = bCitiesAreIndependent;
+	this->hasShield = bHasShield;
+	this->edges = Edges;
+}
+
 Tile::~Tile() {
 
 }
