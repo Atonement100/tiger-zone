9851ee06b823fe06b73781458680d2d9d0ecb520
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-13 21:15:50 -0500

    beginning of port..

--- Tile.java
+++ Tile.java
@@ -1,17 +1,55 @@
+import java.util.ArrayList;
 
 public class Tile {
 	
 	private static int identify = 0;
 	
-	int ID;
+	int ID,
+		rotations;
+	int[] edgeValues;
 	Edge[] edges;
 	Node middle;
-	
+	char tileType;
+	boolean hasMonastery,
+		 	roadsEnd,
+			citiesAreIndependent,
+			hasShield;
+
+
 	public Tile(Edge[] edges, Node middle){
 		this.edges = edges;
 		this.middle = middle;
 		this.connectNodes();
 		this.ID = ++identify;
+	}
+
+	public Tile(boolean hasMonastery, boolean roadsEnd, boolean citiesAreIndependent, boolean hasShield, int[] edges, char tileType){
+		this.hasMonastery = hasMonastery;
+		this.roadsEnd = roadsEnd;
+		this.citiesAreIndependent = citiesAreIndependent;
+		this.hasShield = hasShield;
+		this.tileType = tileType;
+		this.rotations = 0;
+		this.edgeValues = edges;
+
+		if (this.hasMonastery) this.middle = new Node('M');
+		else this.middle = new Node('n'); //none
+
+		//Count number of road + city edges
+		int numRoads = 0, numCities = 0;
+		ArrayList<Integer> cityLocations = new ArrayList<Integer>();
+		for (int edgeIndex = 0; edgeIndex < edges.length; edgeIndex++){
+			if (edges[edgeIndex] == FeatureTypeEnum.City.toInt()){
+				numCities++;
+				cityLocations.add(edgeIndex);
+			}
+			else if (edges[edgeIndex] == FeatureTypeEnum.Road.toInt()){
+				numRoads++;
+			}
+		}
+
+
+
 	}
 	
 	public void connectNodes(){
