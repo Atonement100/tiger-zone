2a3dd29366d0cb8d5a950b30c03e10342daec437
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-13 20:01:43 -0500

    Add relevant things to include inner city and road end feature types

--- FeatureTypeEnum.java
+++ FeatureTypeEnum.java
@@ -1,8 +1,10 @@
 public enum FeatureTypeEnum {
     Field,
     Road,
+    RoadEnd,
     City,
     Wall,
+    InnerWall,
     Monastery,
     None;
 
@@ -10,8 +12,10 @@ public enum FeatureTypeEnum {
         switch(this){
             case Field: return 'F';
             case Road: return 'R';
+            case RoadEnd: return 'E';
             case City: return 'C';
             case Wall: return 'W';
+            case InnerWall: return 'I';
             case Monastery: return 'M';
             case None: return 'X';
             default: throw new IllegalStateException();
