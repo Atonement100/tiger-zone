887434572e3a22c217da9fa3e67dfdba7fc6a668
bobbieisaly <bobbieisaly@gmail.com>
2016-11-15 14:30:30 -0500

    Adding JUnit testing

---
+++ MeepleTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeepleTest {
+
+	@Test
+	public void testMeeple() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMeepleInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOwner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+}
