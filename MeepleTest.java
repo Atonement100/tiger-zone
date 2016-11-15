557b7890d637726e09403e7c100872855a1754bf
bobbieisaly <bobbieisaly@gmail.com>
2016-11-15 14:48:16 -0500

    Updated meeple unit test

--- MeepleTest.java
+++ MeepleTest.java
@@ -3,45 +3,38 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MeepleTest {
-
+	
 	@Test
 	public void testMeeple() {
-		fail("Not yet implemented");
+		Meeple m = new Meeple();
+		assertEquals(-1, m.owner);
+		assertEquals(-1, m.coordX);
+		assertEquals(-1, m.coordY);
+		assertEquals(-1, m.status);
 	}
 
 	@Test
 	public void testMeepleInt() {
-		fail("Not yet implemented");
+		Meeple m = new Meeple(1);
+		assertEquals(1, m.owner);
+		assertEquals(-1, m.coordX);
+		assertEquals(-1, m.coordY);
+		assertEquals(0, m.status);
 	}
 
 	@Test
 	public void testUpdateLocation() {
-		fail("Not yet implemented");
+		Meeple m = new Meeple(1);
+		m.updateLocation(1, 1);
+		assertEquals(1, m.coordX);
+		assertEquals(1, m.coordY);
 	}
 
 	@Test
 	public void testUpdateStatus() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetOwner() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetX() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetY() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetStatus() {
-		fail("Not yet implemented");
+		Meeple m = new Meeple(1);
+		m.updateStatus(1);
+		assertEquals(1, m.status);
 	}
 
 }
