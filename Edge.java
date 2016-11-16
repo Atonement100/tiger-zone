15e91c5e422009bdc204c7ee8fe60d7cff5d2560
michaelflorica <floricamichael@gmail.com>
2016-11-16 13:10:05 -0500

    no wall-farm placement

--- Edge.java
+++ Edge.java
@@ -14,16 +14,12 @@ public class Edge {
             //handle false positives first
             if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.City) continue;
             else if(this.nodes[i].featureType == FeatureTypeEnum.City && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
-            else if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.Field) continue;
-            else if(this.nodes[i].featureType == FeatureTypeEnum.Field && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
             else if(this.nodes[i].featureType == FeatureTypeEnum.InnerWall && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
             else if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.InnerWall) continue;
             else if(this.nodes[i].featureType != other.nodes[2-i].featureType){
                 edgeIsCompatible = false;
             }
         }
-        
         return edgeIsCompatible;
-        
     }
 }
