61e38c6a4cd8fc770dc97c9da2e1e4de0c100b5b
michaelflorica <floricamichael@gmail.com>
2016-11-15 20:47:09 -0500

    Handle inner wall to normal wall compatibility

--- Edge.java
+++ Edge.java
@@ -1,25 +1,29 @@
 
 public class Edge {
-	Node[] nodes;
-	
-	public Edge(Node[] nodes){
-		this.nodes = nodes;
-	}
-	
-	public boolean isCompatible(Edge other){
-		
-		boolean edgeIsCompatible = true;
-		for(int i = 0; i < nodes.length; i++){
-			if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.City) continue;				//false signal, valid placement
-			else if(this.nodes[i].featureType == FeatureTypeEnum.City && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;		//false signal, valid placement
-			else if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.Field) continue;
-			else if(this.nodes[i].featureType == FeatureTypeEnum.Field && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
-			else if(this.nodes[i].featureType != other.nodes[2-i].featureType){
-				edgeIsCompatible = false;
-			}
-		}
-		
-		return edgeIsCompatible;
-		
-	}
+    Node[] nodes;
+    
+    public Edge(Node[] nodes){
+        this.nodes = nodes;
+    }
+    
+    public boolean isCompatible(Edge other){
+        
+        boolean edgeIsCompatible = true;
+        for(int i = 0; i < nodes.length; i++){
+            
+            //handle false positives first
+            if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.City) continue;
+            else if(this.nodes[i].featureType == FeatureTypeEnum.City && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
+            else if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.Field) continue;
+            else if(this.nodes[i].featureType == FeatureTypeEnum.Field && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
+            else if(this.nodes[i].featureType == FeatureTypeEnum.InnerWall && other.nodes[2-i].featureType == FeatureTypeEnum.Wall) continue;
+            else if(this.nodes[i].featureType == FeatureTypeEnum.Wall && other.nodes[2-i].featureType == FeatureTypeEnum.InnerWall) continue;
+            else if(this.nodes[i].featureType != other.nodes[2-i].featureType){
+                edgeIsCompatible = false;
+            }
+        }
+        
+        return edgeIsCompatible;
+        
+    }
 }
