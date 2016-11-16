34e11fcc71c3c5a2b4d7f0b781d9ac0ca1ad400b
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-15 20:35:11 -0500

    Tile tests

---
+++ TileTest.java
@@ -0,0 +1,115 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileTest {
+    Tile allTrueTile = new Tile(true, true, true, 1, new Integer[]{0, 1, 2, 2}, 'T');
+    Tile allFalseTile = new Tile(false, false, false, 2, new Integer[]{2, 1, 0, 1}, 'F');
+
+    @Test
+    public void ctorTrueTileWorks() throws Exception {
+        assertTrue(allTrueTile.hasMonastery);
+        assertTrue(allTrueTile.citiesAreIndependent);
+        assertTrue(allTrueTile.roadsEnd);
+        assertTrue(allTrueTile.animalType == 1);
+    }
+
+    @Test
+    public void ctorFalseTileWorks() throws Exception {
+        assertFalse(allTrueTile.animalType == 2);
+        assertFalse(allFalseTile.hasMonastery);
+        assertFalse(allFalseTile.citiesAreIndependent);
+        assertFalse(allFalseTile.roadsEnd);
+    }
+
+    @Test
+    public void ctorEdgesWorks() throws Exception {
+        assertArrayEquals(allTrueTile.edgeValues, new Integer[]{0,1,2,2});
+        assertArrayEquals(allFalseTile.edgeValues, new Integer []{2,1,0,1});
+    }
+
+    @Test
+    public void ctorTileTypeWorks() throws Exception {
+        assertEquals(allFalseTile.tileType, 'F');
+        assertNotEquals(allFalseTile.tileType, 'T');
+    }
+
+    @Test
+    public void isEqual() throws Exception {
+        assertFalse(allFalseTile.isEqual(allTrueTile));
+        assertTrue(allTrueTile.isEqual(allTrueTile));
+    }
+
+    @Test
+    public void rotateClockwise1() throws Exception {
+        allTrueTile.rotateClockwise(1);
+        assertArrayEquals(allTrueTile.edgeValues, new Integer[]{2,0,1,2});
+    }
+
+    @Test
+    public void rotateClockwise2() throws Exception {
+        allTrueTile.rotateClockwise(2);
+        assertArrayEquals(allTrueTile.edgeValues, new Integer[]{2,2,0,1});
+    }
+
+    @Test
+    public void rotateClockwise3() throws Exception {
+        allTrueTile.rotateClockwise(3);
+        assertArrayEquals(allTrueTile.edgeValues, new Integer[]{1,2,2,0});
+    }
+
+    @Test
+    public void rotateClockwise4() throws Exception {
+        allTrueTile.rotateClockwise(4);
+        assertArrayEquals(allTrueTile.edgeValues, new Integer[]{0,1,2,2});
+    }
+
+    @Test
+    public void rotateClockwise5() throws Exception {
+        Tile trueCopy = allTrueTile;
+        allTrueTile.rotateClockwise(5);
+        trueCopy.rotateClockwise(1);
+        assertArrayEquals(allTrueTile.edgeValues, trueCopy.edgeValues);
+    }
+
+
+    @Test
+    public void rotateAntiClockwise1() throws Exception {
+        Tile trueCopy = allTrueTile;
+        allTrueTile.rotateClockwise(-1);
+        trueCopy.rotateClockwise(3);
+        assertArrayEquals(allTrueTile.edgeValues, trueCopy.edgeValues);
+    }
+
+    @Test
+    public void rotateAntiClockwise3() throws Exception {
+        Tile trueCopy = allTrueTile;
+        allTrueTile.rotateClockwise(-3);
+        trueCopy.rotateClockwise(1);
+        assertArrayEquals(allTrueTile.edgeValues, trueCopy.edgeValues);
+    }
+
+    @Test
+    public void nodeConnectionTileTypeK() throws Exception {
+        Tile tile = new Tile(false, false, false, 0, new Integer[]{2,0,2,0}, 'K');
+        Edge[] edges = tile.edges;
+
+        assertSame(edges[0].nodes[0].neighbors.get(0), edges[2].nodes[2]);
+        assertSame(edges[0].nodes[1].neighbors.get(0), edges[2].nodes[1]);
+        assertSame(edges[0].nodes[2].neighbors.get(0), edges[2].nodes[0]);
+
+        assertSame(edges[1].nodes[0].neighbors.get(0), edges[1].nodes[1]);
+        assertSame(edges[1].nodes[1].neighbors.get(0), edges[1].nodes[0]);
+        assertSame(edges[1].nodes[1].neighbors.get(1), edges[1].nodes[2]);
+        assertSame(edges[1].nodes[2].neighbors.get(0), edges[1].nodes[1]);
+
+        assertSame(edges[2].nodes[0].neighbors.get(0), edges[0].nodes[2]);
+        assertSame(edges[2].nodes[1].neighbors.get(0), edges[0].nodes[1]);
+        assertSame(edges[2].nodes[2].neighbors.get(0), edges[0].nodes[0]);
+
+        assertSame(edges[3].nodes[0].neighbors.get(0), edges[3].nodes[1]);
+        assertSame(edges[3].nodes[1].neighbors.get(0), edges[3].nodes[0]);
+        assertSame(edges[3].nodes[1].neighbors.get(1), edges[3].nodes[2]);
+        assertSame(edges[3].nodes[2].neighbors.get(0), edges[3].nodes[1]);
+    }
+}
