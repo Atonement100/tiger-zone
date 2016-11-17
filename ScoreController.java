873ce2b778239316bf95d2ea840541ca48ff257c
michaelflorica <floricamichael@gmail.com>
2016-11-16 21:55:02 -0500

    ScoreController quick Update
    Thinking of a way to generalize it to Wall and Road cycles

--- ScoreController.java
+++ ScoreController.java
@@ -1,57 +1,189 @@
-import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.ArrayList;
 public class ScoreController {
-	int player1Score;
-	int player2Score;
-	
-	ScoreController(){
-		this.player1Score = 0;
-		this.player2Score = 0;
-	}
-	
-	public void scoreTile(Tile toCheck){
-		
-		ArrayList<Node> toUnvisit = new ArrayList<Node>();
-		
-		Node start = toCheck.edges[1].nodes[1];;
-		start.visited= true;
-		toUnvisit.add(start);
-		
-		
-		
-		Stack<Node> stack = new Stack<Node>();
-		start.neighbors.get(1).visited = true;
-		
-		Node nextTo = start.neighbors.get(1);
-		toUnvisit.add(start.neighbors.get(1));
-		
-		for(int i = 0; i < nextTo.neighbors.size(); i++){
-			if(!nextTo.neighbors.get(i).visited && nextTo.neighbors.get(i).featureType.toChar() == 'W')
-			stack.add(nextTo.neighbors.get(i));
-			toUnvisit.add(nextTo.neighbors.get(i));
-		}
-
-		boolean cycle = false;
-		while(!stack.isEmpty()){
-			Node buffer = stack.pop();
-			System.out.println(buffer.featureType.toChar());
-			buffer.visited = true;
-			toUnvisit.add(buffer);
-			for(int i = 0; i < buffer.neighbors.size(); i++){
-				if(buffer.neighbors.get(i).visited && buffer.neighbors.get(i) == start){
-					cycle = true;
-				}
-				else if(!buffer.neighbors.get(i).visited && buffer.neighbors.get(i).featureType.toChar() == 'W'){
-					stack.add(buffer.neighbors.get(i));
-				}
-			}
-		}
-		
-		for(int i = 0; i < toUnvisit.size(); i++){
-			toUnvisit.get(i).visited = false;
-		}
-		
-		System.out.println(cycle ? "cycle":"no cycle");
-		
-	}
+    int player1Score;
+    int player2Score;
+    
+    ScoreController(){
+        this.player1Score = 0;
+        this.player2Score = 0;
+    }
+    
+    public void attemptScoring(Tile toCheck){
+        
+        ArrayList<Node> wallCycle = getWallCycleNodes(toCheck.edges[0].nodes[0]);
+        for(int i = 0; i < wallCycle.size(); i++){
+            System.out.println(wallCycle.get(i));
+        }
+        
+        
+    }
+    
+    public ArrayList<Node> getWallCycleNodes(Node start){
+        
+        //tentative list of nodesInCycle
+        ArrayList<Node> nodesInCycle = new ArrayList<Node>();
+        
+        //first mark start as visited, and add it in the cycle list
+        start.visited = true;
+        nodesInCycle.add(start);
+        
+        //get one of start node's neighbors of the same feature type and add it in the cycle list
+        for(int neighborIndex = 0; neighborIndex < start.neighbors.size(); neighborIndex++)
+        {
+            if(start.neighbors.get(neighborIndex).featureType.toChar() == 'W' || start.neighbors.get(neighborIndex).featureType.toChar() == 'I')
+            {
+                nodesInCycle.add(start.neighbors.get(neighborIndex));
+                start.neighbors.get(neighborIndex).visited = true;
+                break;
+            }
+            
+        }
+        
+        //if there were no neighbors of the same feature type cycle is impossible,
+        // mark start node as unvisited and return an empty cycle List
+        if(nodesInCycle.size() < 2)
+        {
+            nodesInCycle.remove(0).visited = false;				//this would be the starting node; remove it from the list, unvisit, and return
+            return nodesInCycle;
+        }
+        
+        //get last node added (one node away from starting node)
+        Node oneLevelDepthNode = nodesInCycle.get(nodesInCycle.size()-1);
+        
+        //get one of the one level depth node's neighbors of the same feature type and add it in the cycle list
+        for(int neighborIndex = 0; neighborIndex < oneLevelDepthNode.neighbors.size(); neighborIndex++)
+        {
+            if(!oneLevelDepthNode.neighbors.get(neighborIndex).visited &&
+               (oneLevelDepthNode.neighbors.get(neighborIndex).featureType.toChar() == 'W'
+                ||oneLevelDepthNode.neighbors.get(neighborIndex).featureType.toChar() == 'I'))
+            {
+                nodesInCycle.add(oneLevelDepthNode.neighbors.get(neighborIndex));
+                oneLevelDepthNode.neighbors.get(neighborIndex).visited = true;
+                break;
+            }
+        }
+        
+        //if there were no neighbors of the same feature type cycle is impossible,
+        //mark everything in cycle List unvisited and return an empty List
+        if(nodesInCycle.size() < 3){
+            while(!nodesInCycle.isEmpty()){
+                nodesInCycle.remove(0).visited = false;
+            }
+            return nodesInCycle;						//empty
+        }
+        
+        //get last node added(two nodes away from starting node)
+        //we will start searching at this point, so we will remove it from the nodesInCycle list
+        //so that it doesn't exist two times in the list when it gets added back in the traversal
+        Node twoLevelDepthNode = nodesInCycle.remove(nodesInCycle.size()-1);
+        
+        //create queue to traverse along edges
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(twoLevelDepthNode);
+        
+        boolean cycle = false;
+        while(!queue.isEmpty() && !cycle){
+            Node buffer = queue.poll();
+            buffer.visited = true;
+            nodesInCycle.add(buffer);
+            for(int i = 0; i < buffer.neighbors.size(); i++){
+                if(buffer.neighbors.get(i).visited && buffer.neighbors.get(i) == start)
+                {
+                    cycle = true;
+                }
+                else if(!buffer.neighbors.get(i).visited &&
+                        (buffer.neighbors.get(i).featureType.toChar() == 'W' || buffer.neighbors.get(i).featureType.toChar() == 'I'))
+                {
+                    queue.add(buffer.neighbors.get(i));
+                }
+            }
+        }
+        
+        return nodesInCycle;		//actual cycle;
+        
+    }
+    
+    public ArrayList<Node> getRoadCycleNodes(Node start){
+        
+        //tentative list of nodesInCycle
+        ArrayList<Node> nodesInCycle = new ArrayList<Node>();
+        
+        //first mark start as visited, and add it in the cycle list
+        start.visited = true;
+        nodesInCycle.add(start);
+        
+        //get one of start node's neighbors of the same feature type and add it in the cycle list
+        for(int neighborIndex = 0; neighborIndex < start.neighbors.size(); neighborIndex++)
+        {
+            if(start.neighbors.get(neighborIndex).featureType.toChar() == 'R')
+            {
+                nodesInCycle.add(start.neighbors.get(neighborIndex));
+                start.neighbors.get(neighborIndex).visited = true;
+                break;
+            }
+            
+        }
+        
+        //if there were no neighbors of the same feature type cycle is impossible,
+        // mark start node as unvisited and return an empty cycle List
+        if(nodesInCycle.size() < 2)
+        {
+            nodesInCycle.remove(0).visited = false;				//this would be the starting node; remove it from the list, unvisit, and return
+            return nodesInCycle;
+        }
+        
+        //get last node added (one node away from starting node)
+        Node oneLevelDepthNode = nodesInCycle.get(nodesInCycle.size()-1);
+        
+        //get one of the one level depth node's neighbors of the same feature type and add it in the cycle list
+        for(int neighborIndex = 0; neighborIndex < oneLevelDepthNode.neighbors.size(); neighborIndex++)
+        {
+            if(!oneLevelDepthNode.neighbors.get(neighborIndex).visited && oneLevelDepthNode.neighbors.get(neighborIndex).featureType.toChar() == 'R')
+            {
+                nodesInCycle.add(oneLevelDepthNode.neighbors.get(neighborIndex));
+                oneLevelDepthNode.neighbors.get(neighborIndex).visited = true;
+                break;
+            }
+        }
+        
+        //if there were no neighbors of the same feature type cycle is impossible,
+        //mark everything in cycle List unvisited and return an empty List
+        if(nodesInCycle.size() < 3){
+            while(!nodesInCycle.isEmpty()){
+                nodesInCycle.remove(0).visited = false;
+            }
+            return nodesInCycle;						//empty
+        }
+        
+        //get last node added(two nodes away from starting node)
+        //we will start searching at this point, so we will remove it from the nodesInCycle list
+        //so that it doesn't exist two times in the list when it gets added back in the traversal
+        Node twoLevelDepthNode = nodesInCycle.remove(nodesInCycle.size()-1);
+        
+        //create queue to traverse along edges 
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(twoLevelDepthNode);
+        
+        boolean cycle = false;
+        while(!queue.isEmpty() && !cycle){
+            Node buffer = queue.poll();
+            buffer.visited = true;
+            nodesInCycle.add(buffer);
+            for(int i = 0; i < buffer.neighbors.size(); i++){
+                if(buffer.neighbors.get(i).visited && buffer.neighbors.get(i) == start){
+                    cycle = true;
+                }
+                else if(!buffer.neighbors.get(i).visited && buffer.neighbors.get(i).featureType.toChar() == 'R'){
+                    queue.add(buffer.neighbors.get(i));
+                }
+            }
+        }
+        
+        return nodesInCycle;		//actual cycle;
+    }
+    
+    
+    
 }
