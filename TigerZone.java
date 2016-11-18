344f9949f21374f6a7d42b635f6f1478839d04e9
darshil24 <pateldarshil24@gmail.com>
2016-11-18 15:58:36 -0500

    Added Unfinished GUI
    added basic Unfinished GUI

--- TigerZone.java
+++ TigerZone.java
@@ -1,9 +1,14 @@
+
+import Gui.LabelDemo;
+
+import javax.swing.*;
 import java.util.*;
 
 public class TigerZone {
 
 
 	public static void main(String[] args){
+		LabelDemo l= new LabelDemo();
 		System.out.print("Enter number of human players: ");
 		Scanner scanner = new Scanner(System.in);
 		int numPlayers = scanner.nextInt();
