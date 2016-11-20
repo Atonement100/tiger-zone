94393df86141315f534765c2ffacd230eee8ab2d
darshil24 <pateldarshil24@gmail.com>
2016-11-20 15:11:30 -0500

    Preview Window improvement
    preview pannel is improved with rotate left and right pannel.
    First tile is place on the middle of the gird.

--- Gui/PreView.java
+++ Gui/PreView.java
@@ -2,6 +2,8 @@ package Gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Darshil on 11/19/2016.
@@ -10,8 +12,7 @@ public class PreView extends JFrame{
    public JFrame preview = new JFrame("next tile");
    public JLabel previewLabel = new JLabel();
    public JPanel previewPanel = new JPanel();
-    public JButton rotateLeft = new JButton();
-    public JButton rotateRight = new JButton();
+
     public ImageIcon img;
     public String imgID;
 
@@ -32,17 +33,38 @@ public class PreView extends JFrame{
         previewLabel.setIcon(img);
     }
 
-    public PreView() throws HeadlessException {
-        previewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        previewLabel.setVerticalAlignment(SwingConstants.TOP);
-        rotateLeft.setText("Rotate Left");
-        rotateRight.setText("Rotate Right");
+    public PreView()  {
+        MouseAdapter ma = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(((JButton)e.getSource()).getText());
+            }
+        };
+         JButton rotateLeft = new JButton("Rotate left");
+            rotateLeft.setBackground(Color.WHITE);
+        rotateLeft.setBounds(20,125,100,25);
+        rotateLeft.addMouseListener(ma);
+        JButton rotateRight = new JButton("Rotate right");
+        rotateRight.setBackground(Color.WHITE);
+        rotateRight.addMouseListener(ma);
+        rotateRight.setBounds(150,125,100,25);
+        previewLabel.setBounds(110,25,50,50);
+//        previewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//        previewLabel.setVerticalAlignment(SwingConstants.TOP);
 
+     //  rotateLeft.setVerticalAlignment(SwingConstants.BOTTOM);
+//        rotateRight.setHorizontalAlignment(SwingConstants.BOTTOM);
+//        rotateRight.setVerticalAlignment(SwingConstants.RIGHT);
+
+       // rotateRight.setText("Rotate Right");
+            preview.add(rotateLeft);
+           preview.add(rotateRight);
+        preview.add(previewLabel);
       // preview.add(previewPanel.add(rotateRight));
 
         preview.setSize(300, 300);
 
-        preview.add(previewPanel.add(previewLabel));
+        preview.add(previewPanel);
         preview.setVisible(true);
     }
 }
