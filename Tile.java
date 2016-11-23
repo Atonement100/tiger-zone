db084162fb815f4c1678fdfc008bc409b0375578
michaelflorica <floricamichael@gmail.com>
2016-11-23 18:18:31 -0500

    Fix single Tile Print

--- Tile.java
+++ Tile.java
@@ -289,9 +289,9 @@ public class Tile {
             System.out.print(toPrint.edges[1].nodes[i].featureType.toChar());
         }
         System.out.println("");
-        System.out.println(toPrint.edges[0].nodes[2].featureType.toChar() + "   " + toPrint.edges[2].nodes[2].featureType.toChar());
+        System.out.println(toPrint.edges[0].nodes[2].featureType.toChar() + "   " + toPrint.edges[2].nodes[0].featureType.toChar());
         System.out.println(toPrint.edges[0].nodes[1].featureType.toChar() + "   " + toPrint.edges[2].nodes[1].featureType.toChar());
-        System.out.println(toPrint.edges[0].nodes[0].featureType.toChar() + "   " + toPrint.edges[2].nodes[0].featureType.toChar());
+        System.out.println(toPrint.edges[0].nodes[0].featureType.toChar() + "   " + toPrint.edges[2].nodes[2].featureType.toChar());
         System.out.print(" ");
         for(int i = 0; i < 3; i++){
             System.out.print(toPrint.edges[3].nodes[i].featureType.toChar());
