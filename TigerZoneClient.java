544d7e911e51c4e1cf40c6a06fb7e94416ae8b46
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-28 03:06:35 -0500

    Revert "Update TigerZoneClient.java"
    This reverts commit 22da2c07d697773f8cb350021875e16d265ddf1f.

--- TigerZoneClient.java
+++ TigerZoneClient.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class TigerZoneClient {
+public class TigerZoneNClient {
     public static void main(String[] args) throws IOException {
         
         if (args.length != 5) {
