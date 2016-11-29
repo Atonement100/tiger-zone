164df3107b73d0849291f2175a282af529ee33b6
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-11-29 11:24:12 -0500

    fix meeple placement

--- GameBoard.java
+++ GameBoard.java
@@ -61,7 +61,7 @@ public class GameBoard {
             }
         }
 
-        updateMeepleInfoForNewTile(targetLocation);
+        updateMeepleInfoForTemporaryTile(targetLocation);
     }
 
     void removeTemporaryTile(Location targetLocation){
@@ -114,7 +114,7 @@ public class GameBoard {
                         if (currNode.featureType.isSameFeature(neighbor.featureType)) {
                             if (neighbor.meeplePlacedInFeature) {
                                 shouldMarkVisited = true;
-                            } else if (!visitedQueue.contains(neighbor) && neighbor.owningTileId == currNode.owningTileId) {     //Only add to nodequeue if we haven't seen it before
+                            } else if (!visitedQueue.contains(neighbor)) {     //Only add to nodequeue if we haven't seen it before
                                 nodeQueue.add(neighbor);
                             }
                         }
@@ -147,7 +147,7 @@ public class GameBoard {
                     Node currNode = nodeQueue.removeFirst();
                     visitedQueue.add(currNode);
                     for (Node neighbor : currNode.neighbors) {
-                        if (currNode.featureType.isSameFeature(neighbor.featureType)) {
+                        if (currNode.featureType.isSameFeature(neighbor.featureType) && neighbor.owningTileId == currNode.owningTileId) {
                             if (neighbor.meeplePlacedInFeature) {
                                 shouldMarkVisited = true;
                             } else if (!visitedQueue.contains(neighbor)) {     //Only add to nodequeue if we haven't seen it before
