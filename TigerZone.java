a424fcb3e6d8c185848d4c436c84ed7d7593cb46
Timothy Russell-Wagner <atonement100@gmail.com>
2016-12-02 11:06:50 -0500

    Small cleanup

--- TigerZone.java
+++ TigerZone.java
@@ -1,9 +1,6 @@
-import javax.swing.*;
 import java.util.*;
 
 public class TigerZone {
-
-
 	public static void main(String[] args){
 		System.out.print("Enter number of human players: ");
 		Scanner scanner = new Scanner(System.in);
